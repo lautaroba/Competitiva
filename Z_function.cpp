a58105ec03bc5bdf68e47d6021caa013d80033d0
lautaroba <86685579+lautaroba@users.noreply.github.com>
2021-06-29 16:22:24 -0300

    Create Z_function.cpp

---
+++ Z_function.cpp
@@ -0,0 +1,11 @@
+vector<int> Zfunction(const string &s){
+	int n = s.size();
+	vector<int> p(n);
+	for (int i = 1; i < n; i++) {
+		int borde = p[i - 1];
+		while(borde>0 and s[i]!=s[borde])
+			borde = p[borde - 1];
+		p[i] = borde + (s[i] == s[borde]);
+	}
+	return p;
+}
