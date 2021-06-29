b5eba6066a4c906a6f16f4ae71a386d51251abdd
lautaroba <86685579+lautaroba@users.noreply.github.com>
2021-06-29 16:22:58 -0300

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define forr(i,a,b) for(int i=(a);i<(b);i++)
+#define forn(i,n) forr(i,0,n)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();it++)
+#define pb push_back
+#define fst first
+#define snd second
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
+int main()
+{
+	//freopen("prueba.txt", "w", stdout);
+	//freopen("entrada.txt", "r", stdin);
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	
+	
+	return 0;
+}
