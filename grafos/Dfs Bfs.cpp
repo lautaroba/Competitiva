029ffce9128045aa19cac0066cf64b8866afc22d
lautaroba <86685579+lautaroba@users.noreply.github.com>
2021-10-03 15:03:10 -0300

    Create Dfs Bfs.cpp

---
+++ grafos/Dfs Bfs.cpp
@@ -0,0 +1,41 @@
+
+///////////////////////////////////////////////////////////////////
+//                               DFS
+vector<vector<int>> adj; // graph represented as an adjacency list
+int n; // number of vertices
+
+vector<bool> visited;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adj[v]) {
+        if (!visited[u])
+            dfs(u);
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+//                               BFS
+vector<vector<int>> adj;  // adjacency list representation
+int n; // number of nodes
+int s; // source vertex
+
+queue<int> q;
+vector<bool> used(n);
+vector<int> d(n), p(n);
+
+q.push(s);
+used[s] = true;
+p[s] = -1;
+while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+    for (int u : adj[v]) {
+        if (!used[u]) {
+            used[u] = true;
+            q.push(u);
+            d[u] = d[v] + 1;
+            p[u] = v;
+        }
+    }
+}
